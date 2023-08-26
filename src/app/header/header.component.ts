a4bc764c0d2fe3c3b1470f6cf1de1b4ebb450280
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-08-26 14:49:44 +0100

    Add header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth/shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  faUser = faUser;
+  isLoggedIn: boolean;
+  username: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) { 
+    this.isLoggedIn = false;
+    this.username = '';
   }
 
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUserName();
+  }
+
+  goToUserProfile() {
+    this.router.navigateByUrl('/user-profile/' + this.username);
+  }
+
+  logout() {
+    this.authService.logoutHandleHTTP();
+    this.isLoggedIn = false;
+    this.router.navigateByUrl('');
+  }
 }
