5843e76c319e00b4a8dd29dcd4336fcf338e4b94
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-08-26 02:32:45 +0100

    Change Backend EndPoint

--- src/app/globlal-variable.ts
+++ src/app/globlal-variable.ts
@@ -2,7 +2,7 @@ export const GlobalVariable = Object.freeze({
     
     API_ENDPOINTS: "http://localhost:8600",
 
-   // API_ENDPOINTS: "http://confession.eu-west-2.elasticbeanstalk.com"
-
+   // TERMINATED API_ENDPOINTS: "http://confession.eu-west-2.elasticbeanstalk.com". 
+   // API_ENDPOINTS  http://jamesvo.eu-west-2.elasticbeanstalk.com/
 
 });
