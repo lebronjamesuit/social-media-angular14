ec54c6932202f55874073e15088ec9791a18431e
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-08-29 21:42:20 +0100

    Post tile component

---
+++ src/app/shared/post-tile/post-tile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { PostModel } from '../post-model';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-post-tile',
+  templateUrl: './post-tile.component.html',
+  styleUrls: ['./post-tile.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class PostTileComponent implements OnInit {
+
+  faComments = faComments;
+  @Input() posts$!: PostModel[];
+
+  constructor(private router: Router) { 
+    console.log('constructor ' + this.posts$ )
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit ' + this.posts$ )
+    this.posts$.forEach( x => console.log("PostTileComponent ID is " +  x.id));
+  }
+
+  goToPost(id: number): void {
+    this.router.navigateByUrl('/view-post/' + id);
+  }
+}
